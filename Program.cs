bb1acd96f06d6b850ea2d6ce2799a395083fca0a
MickaelHm <harmeau.mickael@gmail.com>
2018-12-17 15:18:52 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Ecrivez votre nom suivi de votre prénom puis appuyez sur entrée");
+            name = Console.ReadLine();
+            Console.WriteLine("Bonjour " + name + ", nous somme le " + DateTime.Now.ToLongDateString() + ", comment allez-vous?") ;
+
+
+           
+            
+        }
+    }
+}
